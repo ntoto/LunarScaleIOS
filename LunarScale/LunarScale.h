1cee0979015a220d8a1725663b3b97bd39c2b1d4
npouvesle <nicolas.pouvesle@gmail.com>
2017-10-21 18:18:47 -0700

    Initial Commit

---
+++ LunarScale/LunarScale.h
@@ -0,0 +1,19 @@
+//
+//  LunarScale.h
+//  LunarScale
+//
+//  Created by Nicolas Pouvesle on 10/21/17.
+//  Copyright © 2017 Nicolas Pouvesle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LunarScale.
+FOUNDATION_EXPORT double LunarScaleVersionNumber;
+
+//! Project version string for LunarScale.
+FOUNDATION_EXPORT const unsigned char LunarScaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LunarScale/PublicHeader.h>
+
+
